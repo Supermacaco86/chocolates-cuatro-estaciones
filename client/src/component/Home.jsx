5c5d32d888c959786599a188b2f767645e299fb3
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-12 21:32:18 -0300

    LandingPage Home App ruteo

---
+++ client/src/component/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+ 
+
+export default function home (){
+    return(
+        
+        <div>
+            <h1 >Bienvenido a Chocolates Cuatro Estaciones!</h1>
+        </div>
+        
+    )
+}
