7b4146a5f3832320ed9dea5a1ce712c5dfec875c
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-12 21:32:18 -0300

    LandingPage Home App ruteo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from "react";
+import {Route} from "react-router-dom";
+import {Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+import LandingPage from './component/LandingPage';
+import Home from './component/Home';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Henry Countries</h1>
+      <Routes>
+        <Route exact path='/' element={<LandingPage/>}/>
+        <Route path='/home' exact element={<Home/>}/> 
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
