fcc9c2fde72584cc293fc8110f07ced8e5491295
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-15 22:46:27 -0300

    prvider y componente authContext

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import {Route} from "react-router-dom";
 import {Routes} from "react-router-dom";
 import {BrowserRouter} from "react-router-dom";
+import { AuthProvider } from './context/authContext';
 import LandingPage from './component/LandingPage';
 import Home from './component/Home';
 import Login from './component/Login';
@@ -13,12 +14,14 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <Routes>
-        <Route exact path='/' element={<LandingPage/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/home' exact element={<Home/>}/> 
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route exact path='/' element={<LandingPage/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/home' exact element={<Home/>}/> 
+        </Routes>
+      </AuthProvider>
     </div>
     </BrowserRouter>
   );
