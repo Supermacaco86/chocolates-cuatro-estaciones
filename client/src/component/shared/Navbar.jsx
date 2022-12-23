ba77c78ea6bab027dd72bea05804cc40bac58af7
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-22 23:53:23 -0300

    navbar sin texto

--- client/src/component/shared/Navbar.jsx
+++ client/src/component/shared/Navbar.jsx
@@ -3,16 +3,16 @@ import {Link} from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar
-         <Link to = "/register">
-                <button>Registrarse</button>
-            </Link>
-            <Link to = "/login">
-                <button>Login</button>
-            </Link>
-            <Link to = "/home">
-                <button>Home</button>
-            </Link>
+    <div>
+        <Link to = "/register">
+            <button>Registrarse</button>
+        </Link>
+        <Link to = "/login">
+            <button>Login</button>
+        </Link>
+        <Link to = "/home">
+            <button>Home</button>
+        </Link>
     </div>
   )
 }
