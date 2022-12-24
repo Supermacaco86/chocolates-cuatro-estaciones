c47094f374c31af1531986dad5ef2857cb298173
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-24 00:08:43 -0300

    texto landingPage

--- client/src/component/LandingPage.jsx
+++ client/src/component/LandingPage.jsx
@@ -5,6 +5,22 @@ export default function LandingPage (){
         
         <div>
             <h1 >Bienvenido a Chocolates Cuatro estaciones!</h1>
+            <h2>El chocolate es mi pasion!</h2>
+            <h3>Mi nombre es Santiago Figueroa, soy Chocolatier egresado de la escuela de Ingrid Koock. </h3>
+            <h2>Mis productos</h2>
+            <h3>Barras de chocolate</h3>
+            <h4>Chocolate semiamargo con mani</h4>
+            <h4>Chocolate con leche relleno de dulce de leche</h4>
+            <h4>Chocloate marmolado</h4>
+            <h4>Chocolate blanco con cookies</h4>
+            <h3>Tortas</h3>
+            <h4>Tortas decoradas</h4>
+            <h4>Tortas de diferentes rellenos</h4>
+            <h3>Bombones</h3>
+            <h4>Bombones rellenos de licor</h4>
+            <h4>Bombones de chocolate semiamargo rellenos de dulce de leche</h4>
+            <h4>Bombones de chocolate con leche rellenos de castañas de caju</h4>
+            <h4>Bombones de chocolate blanco</h4>
         </div>
         
     )
