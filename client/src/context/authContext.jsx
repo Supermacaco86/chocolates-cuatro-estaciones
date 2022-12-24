aaadfbf1a4d0489ed6fda0711882d31551cf5600
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-23 23:15:30 -0300

    ruta protejida y login terminado

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,5 +1,8 @@
-import { createContext, useContext } from "react";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { createContext, useContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword,
+         signInWithEmailAndPassword,
+         onAuthStateChanged,
+         signOut } from "firebase/auth"
 import { auth } from "../firebase"
 
 export const authContext = createContext();
@@ -10,12 +13,25 @@ export const useAuth = () => {
 }
 
 export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     const signup = (email, password) => 
     createUserWithEmailAndPassword( auth, email, password )
 
     const login = (email, password) =>
     signInWithEmailAndPassword( auth, email,password)
+
+    const logout = () => signOut(auth);
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => unsubscribe()
+    },[])
     
-    return <authContext.Provider value={{ signup, login }}>{children}</authContext.Provider>;
+    return <authContext.Provider value={{ signup, login, user, logout, loading }}>{children}</authContext.Provider>;
 };
 
