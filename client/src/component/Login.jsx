b8f269cc24faab22b9d95b266ece5bd331e72249
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-23 21:43:06 -0300

    inicio login

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import { useState } from 'react'
+import { useAuth } from '../context/authContext'
+import { useNavigate } from 'react-router-dom';
+import { async } from '@firebase/util';
 
 function Login() {
+
+  const [user, setUser] = useState({
+    email:"",
+    password:"",
+  });
+  const { login } = useAuth()
+  const navigate = useNavigate()
+  const [ error, setError ] = useState()
+
+
+  const handleChange = ({target: {name, value}}) => {
+    setUser({...user, [name]: value})
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    setError("")
+    try {
+      await login( user.email, user.password)
+      navigate('/homelogin')
+    } catch (error) {
+      console.log(error.code)
+      if(error.code === "auth/invalid-email"){
+        setError("Ingrese un correo valido")
+      }else if(error.code === "auth/wrong-password"){
+        setError("La contraseña ingresada es incorrecta")
+      }
+    }
+    
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      { error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input
+        type="text"
+        name="email"
+        placeholder="user@compañy.com"
+        onChange={handleChange}/>
+
+        <label>Password</label>
+        <input
+        type="password"
+        name="password"
+        placeholder="*******"
+        onChange={handleChange}/>
+
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "../firebase"
 
 export const authContext = createContext();
@@ -12,7 +12,10 @@ export const useAuth = () => {
 export function AuthProvider({ children }) {
     const signup = (email, password) => 
     createUserWithEmailAndPassword( auth, email, password )
+
+    const login = (email, password) =>
+    signInWithEmailAndPassword( auth, email,password)
     
-    return <authContext.Provider value={{signup}}>{children}</authContext.Provider>;
+    return <authContext.Provider value={{ signup, login }}>{children}</authContext.Provider>;
 };
 
