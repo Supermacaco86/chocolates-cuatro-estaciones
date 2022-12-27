6a3983f4ff8551ed6af0859573a33f048da486ed
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-27 20:44:18 -0300

    instalacion de boostrap

--- client/src/component/shared/Navbar.jsx
+++ client/src/component/shared/Navbar.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import { useAuth } from '../../context/authContext';
+import bootstrap from 'bootstrap' 
 
 function Navbar() {
   const {logout, loading} = useAuth()
@@ -14,7 +15,7 @@ if(loading) return <h1>Cargando</h1>
 
   return (
     <div>
-        <Link to = "/register">
+        {/* <Link to = "/register">
             <button>Registrarse</button>
         </Link>
         <Link to = "/login">
@@ -23,7 +24,32 @@ if(loading) return <h1>Cargando</h1>
         <Link to = "/home">
             <button>Ingresar</button>
         </Link>
-        <button onClick={handleLogout}>Logout</button>
+        <button onClick={handleLogout}>Logout</button> */}
+        <nav class="navbar navbar-expand-md navbar-light">
+          <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar-toggler">
+              <a class="navbar-brand" href="#">Hidden brand</a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Chocolate es mi pasion</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Mis productos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                  </li>
+                </ul>
+      {/* <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> */}
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
